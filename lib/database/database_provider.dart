532662e8391de954bce690049c8441b4a116c8b1
moisesRubens <moisesrubens.pro@gmail.com>
2026-07-23 12:33:26 -0300

    refactor Order's domain and database_provider.dart

--- lib/database/database_provider.dart
+++ lib/database/database_provider.dart
@@ -2,12 +2,13 @@ import 'package:dairy/domain/product.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'dart:async';
+import 'package:dairy/domain/order2.dart';
 
 class DatabaseProvider {
-  static final DatabaseProvider _instance = DatabaseProvider.internal();
-  factory DatabaseProvider() => _instance;
   Database? _db;
-
+  
+  factory DatabaseProvider() => _instance;
+  static final DatabaseProvider _instance = DatabaseProvider.internal();
   DatabaseProvider.internal();
 
   Future<Database> get db async {
@@ -20,32 +21,72 @@ class DatabaseProvider {
   }
 
   Future<Database> initDb() async {
-    final databasesPath = await getDatabasesPath();
-    final path = join(databasesPath, "dairy.db");
-    
-    return await openDatabase (
-      path,
-      version: 2,
-      onCreate: (Database db, int version) async {
+    try {
+      final databasesPath = await getDatabasesPath();
+      final path = join(databasesPath, "dairy_database.db");
+      
+      return await openDatabase (
+        path,
+        version: 3,
+        onCreate: _onCreate,
+        onUpgrade: _onUpgrade,
+      );
+    } catch(e) {
+      throw Exception('Erro ao inicializar banco de dados: $e');
+    }
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+    try {
+      await db.execute('''
+        CREATE TABLE products (
+          ${Product.idColumn} INTEGER PRIMARY KEY AUTOINCREMENT,
+          ${Product.productIdColumn} INTEGER UNIQUE,
+          ${Product.nameColumn} TEXT NOT NULL,
+          ${Product.priceColumn} REAL,
+          ${Product.amountColumn} INTEGER,
+          ${Product.kgColumn} REAL, 
+          ${Product.litersColumn} REAL,
+          ${Product.dateColumn} TEXT,
+          updated_at TEXT
+        );
+      ''');
+      await db.execute('''
+        CREATE TABLE orders (
+          ${Order.idColumn} INTEGER PRIMARY KEY AUTOINCREMENT,
+          ${Order.orderIdColumn} INTEGER UNIQUE,
+          ${Order.statusColumn} INT NOT NULL,
+          ${Order.totalValueColumn} REAL,
+          ${Order.dateTimeColumn} TEXT NOT NULL,
+          ${Order.descriptionColumn} TEXT, 
+          updated_at TEXT
+        );
+      ''');    
+    } catch(e) {
+      throw Exception('Erro ao criar banco de dados: $e');
+    }
+  }
+
+  Future<void> _onUpgrade(Database db, int oldVersion, int newVersion) async {
+    try{
+      if (oldVersion < 2) {
+        await db.execute('ALTER TABLE products ADD COLUMN ${Product.dateColumn} TEXT');
+      }
+      if (oldVersion < 3) {
         await db.execute('''
-          CREATE TABLE products (
-            ${Product.idColumn} INTEGER PRIMARY KEY AUTOINCREMENT,
-            ${Product.productIdColumn} INTEGER UNIQUE,
-            ${Product.nameColumn} TEXT NOT NULL,
-            ${Product.priceColumn} REAL,
-            ${Product.amountColumn} INTEGER,
-            ${Product.kgColumn} REAL, 
-            ${Product.litersColumn} REAL,
-            ${Product.dateColumn} TEXT,
+          CREATE TABLE orders (
+            ${Order.idColumn} INTEGER PRIMARY KEY AUTOINCREMENT,
+            ${Order.orderIdColumn} INTEGER UNIQUE,
+            ${Order.statusColumn} INT NOT NULL,
+            ${Order.totalValueColumn} REAL,
+            ${Order.dateTimeColumn} TEXT NOT NULL,
+            ${Order.descriptionColumn} TEXT, 
             updated_at TEXT
           );
         ''');
-      },
-      onUpgrade: (Database db, int oldVersion, int newVersion) async {
-        if (oldVersion < 2) {
-          await db.execute('ALTER TABLE products ADD COLUMN ${Product.dateColumn} TEXT');
-        }
-      },
-    );
+      } 
+    } catch(e) {
+      throw Exception('Erro ao atualizar banco de dados: $e');
+    }
   }
 }
