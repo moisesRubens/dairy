2d1f56cf8a2e2473f78ae12ad61c45d6e71262e5
moisesRubens <moisesrubens.pro@gmail.com>
2026-08-26 09:22:45 -0300

    feat: add Product's controller

---
+++ lib/controllers/product_controller.dart
@@ -0,0 +1,47 @@
+import 'package:dairy/domain/product.dart';
+import 'package:dairy/services/product_service.dart';
+import 'package:flutter/material.dart';
+
+class ProductController extends ChangeNotifier
+{
+  List<Product> _list = [];
+  final ProductService _service;
+  bool _isLoading = false;
+
+  ProductController({ProductService? service}) : _service = service ?? ProductService();
+  
+  bool get isLoading => _isLoading;
+
+  Future<bool> add(Product product) async
+  {
+    try
+    {
+      await _service.createProduct(product);
+      await refreshProducts();
+      return true;
+    } 
+    catch(e)
+    {
+      return false;
+    }
+  }
+
+  Future<void> refreshProducts() async 
+  {
+    _isLoading = true;
+    notifyListeners();
+    try 
+    {
+      _list = await _service.getProducts();
+    }
+    catch(e)
+    {
+      print("EXCECAO NO REFRESH PRODUCTS");
+    }
+    finally
+    {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+}
